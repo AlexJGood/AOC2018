6bb680b402d3951eb25d08c67fb5fb188515d06c
Alex Good <AGood@rightpoint.com>
2018-12-05 13:09:03 -0600

    Part2 done

---
+++ Day1/part2.rs
@@ -0,0 +1,61 @@
+use std::{
+    env,
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+    collections::HashMap
+};
+
+fn lines_from_file<P>(filename: P) -> Vec<String>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+fn calculation_frequency(mut current_frequency:i32, already_existing:&mut HashMap<i32,String>,input_lines:&Vec<String>) -> (bool,i32)
+{
+    for line in input_lines {
+            current_frequency += line.parse::<i32>().unwrap();
+            if !already_existing.contains_key(&current_frequency)
+            {
+                already_existing.insert(current_frequency,"".to_string());
+            }
+            else
+            {
+                println!("{} This is the dup", current_frequency);
+                return (true,current_frequency)  
+            }
+        }
+
+    return (false,current_frequency);
+}
+
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let mut _current_frequency = 0;
+    let mut _already_existing = HashMap::new();
+    let mut _found_frequency = false;
+    if args.len() == 2
+        {
+        let lines = lines_from_file(&args[1]);
+         while !_found_frequency
+         {
+             let (found_frequency,current_frequency) = calculation_frequency(_current_frequency,&mut _already_existing,&lines);
+             _found_frequency = found_frequency;
+             _current_frequency = current_frequency;
+             println!("{}", _found_frequency); 
+         }
+         
+         println!("{}", _already_existing.len());    
+         println!("{}", _current_frequency);    
+    }
+}
